ce3e2683bd444704fa37a897b71c743bc52c8e4c
xlooslo <bluearth4587@g.hongik.ac.kr>
2023-07-26 19:24:18 +0900

    Rename: StoryEdit to Story

---
+++ src/Story/Story.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./logo.svg";
+
+function Story() {
+  return (
+    <div>
+      Story
+    </div>
+  );
+}
+
+export default Story;
--- src/Story/StoryEdit.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import logo from "./logo.svg";
-
-function StoryEdit() {
-  return <div></div>;
-}
-
-export default StoryEdit;
