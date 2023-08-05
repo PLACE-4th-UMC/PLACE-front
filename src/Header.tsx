f0c5dacb7317b339cfd9fb877306d49dc554f18a
happyrhkr <happyrhkr@g.hongik.ac.kr>
2023-08-05 20:51:24 +0900

    Feat: add Header.tsx

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import "./Header.css";
 
 function Header() {
-  return <div></div>;
+  return (
+    <header>
+      <div className="header">
+        <div className="header-logo-wrapper">
+          <a href="/" className="header-logo">
+            Place
+          </a>
+          <a href="/" className="header-logo-dot">
+            .
+          </a>
+        </div>
+        <div className="menu">
+          <div className="menuLeft">
+            <a href="/exhibitions">Exhibitions</a>
+            <div className="dropdown">
+              <span>Categories</span>
+              <div className="dropdown-content">
+                <a href="/categories/Design">디자인</a>
+                <a href="/categories/Photo">사진</a>
+                <a href="/categories/Experience">체험</a>
+                <a href="/categories/Install">설치미술</a>
+              </div>
+            </div>
+            <a href="/community">Community</a>
+            <a href="/story">Share Story</a>
+            <a href="/magazine">Place Magazine</a>
+          </div>
+          <div className="menuRight">
+            <a href="/login">Login</a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
