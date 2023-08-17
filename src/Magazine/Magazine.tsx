d9dd54f98c150e5696b57e22ed947ba720e01e0d
inthhh <glass005533@gmail.com>
2023-08-17 11:56:59 +0900

    Feat: Magazine item component

--- src/Magazine/Magazine.tsx
+++ src/Magazine/Magazine.tsx
@@ -4,6 +4,20 @@ import "./magazine.css";
 import Header from "../Header";
 import Footer from "../Footer";
 
+interface ItemProps {
+  // leftMargin: number;
+}
+
+const Item: React.FC<ItemProps> = () => {
+  return (
+    <div style={{ margin: "auto" }}>
+      <div className="item-box"></div>
+      <div className="item-title">아이템 제목</div>
+      <div className="item-address">아이템 주소</div>
+    </div>
+  );
+};
+
 function Magazine() {
   return (
     <div>
@@ -12,6 +26,17 @@ function Magazine() {
       <div style={{ marginBottom: "38px" }}>
         <div className="sub-title">My fav & My scrap</div>
         <div className="red-bar"></div>
+      </div>
+
+      <div style={{ display: "flex", marginLeft: "40px", marginRight: "40px" }}>
+        <Item />
+        <Item />
+        <Item />
+      </div>
+      <div style={{ display: "flex", marginLeft: "40px", marginRight: "40px" }}>
+        <Item />
+        <Item />
+        <Item />
       </div>
       <Footer />
     </div>
