7cbfed51f75b0cadd1a0582fa497f22d47a6406b
inthhh <glass005533@gmail.com>
2023-08-17 10:30:31 +0900

    Feat: start Magazine Page

--- src/Magazine/Magazine.tsx
+++ src/Magazine/Magazine.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./magazine.css";
+import Header from "../Header";
+import Footer from "../Footer";
 
 function Magazine() {
-  return <div>magazine</div>;
+  return (
+    <div>
+      <Header />
+      <div className="magazine-title-box"></div>
+      <div style={{ marginBottom: "38px" }}>
+        <div className="sub-title">My fav & My scrap</div>
+        <div className="red-bar"></div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Magazine;
