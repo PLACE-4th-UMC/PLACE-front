78540433a333ca8b6deebaa1829099bc4d894d5f
inthhh <glass005533@gmail.com>
2023-08-17 10:07:26 +0900

    Feat: complete footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -5,13 +5,40 @@ function Footer() {
   return (
     <div className={Style["footer"]}>
       <div className={Style["bottom"]}>
-        <div className={Style["place"]}>
-          Place<span>.</span>
+        <div>
+          <div className={Style["place"]}>
+            Place<span>.</span>
+          </div>
+          <br />
+          <br />
+          <div style={{ display: "flex" }}>
+            <div className={Style["sns-btn"]} style={{ marginLeft: "80px" }}>
+              PlayStore
+            </div>
+            <div className={Style["sns-btn"]} style={{ marginRight: "101px" }}>
+              AppStore
+            </div>
+          </div>
         </div>
-        <br />
-        <br />
-        <div className={Style["sns-btn"]}>PlayStore</div>
-        <div className={Style["sns-btn"]}>AppStore</div>
+        <div style={{ marginRight: "60px", marginTop: "86px" }}>
+          <div className={Style["menu"]}>COMPANY</div>
+          <div className={Style["menu-list"]}>About Us</div>
+          <div className={Style["menu-list"]}>Legal Information</div>
+          <div className={Style["menu-list"]}>Contact Us</div>
+        </div>
+        <div style={{ marginRight: "60px", marginTop: "86px" }}>
+          <div className={Style["menu"]}>HELP CENTER</div>
+          <div className={Style["menu-list"]}>Find a Property</div>
+          <div className={Style["menu-list"]}>How To Host?</div>
+          <div className={Style["menu-list"]}>Why Us?</div>
+          <div className={Style["menu-list"]}>FAQs</div>
+        </div>
+        <div style={{ marginTop: "86px" }}>
+          <div className={Style["menu"]}>CONTACT INFO</div>
+          <div className={Style["menu-list"]}>Phone: 1234567890</div>
+          <div className={Style["menu-list"]}>Email: company@email.com</div>
+          <div className={Style["menu-list"]}>Locatino: 100 Smart Street, LA, USA</div>
+        </div>
       </div>
     </div>
   );
