cdb1f384e5ed8098c5eb0b136c8bfacfa2dc6ba7
yumzen <yeom.ji.eun35@gmail.com>
2023-08-31 10:56:58 +0900

    Feat: Add Change css

--- src/Exhibitions/categories/design.tsx
+++ src/Exhibitions/categories/design.tsx
@@ -1,29 +1,24 @@
 import { useEffect, useState } from "react";
 import Modal from "../Modal";
-import exhibitiondata from "../exhibitdata";
 import Header from "../../Header";
 import Footer from "../../Footer";
+import axios from "axios";
 
 interface ExhibitItem {
     exhibitionIdx: number;
     exhibitionImg: string;
     exhibitionName: string;
     location: string;
-    startDate : string;
-    endDate : string;
-    operatingTime: string;
-    fee: string;
-    artist: string;
-    rating: string;
-    story: {
-    nickname: string;
-    storyImg: string;
-    userImg: string;
-    heart: boolean | string;
+}
+
+interface APIResponse {
+    isSuccess: boolean;
+    code: number;
+    message: string;
+    result: {
+    content: ExhibitItem[];
     };
-    viewed:string;
-    popular:string;
-    }
+}
 
 function Design() {
     const itemsPerPage = 9;
@@ -34,9 +29,7 @@ function Design() {
     null
     );
     const [modal, setModal] = useState(false);
-    const [exhibitsData, setExhibitsData] = useState<ExhibitItem[]>(
-    exhibitiondata
-    );
+    const [exhibitsData, setExhibitsData] = useState<ExhibitItem[]>([]);
 
 const handleLoadMore = () => {
 setcurrentItemSet((currentItemSet) => currentItemSet + itemsPerPage);
@@ -61,49 +54,74 @@ const toggleModal = () => {
 setModal(!modal);
 };
 
+// 최신순 조회 예시
+const createdDateUrl = `http://exhibition-place.site:8080/exhibitions?searchWord=&categoryName=design&page=&size=&sort=createdDate,desc`;
+// 인기순 조회 예시
+const likeCountUrl = `http://exhibition-place.site:8080/exhibitions?searchWord=&categoryName=design&page=&size=&sort=likeCount,desc`;
+// 조회수순 조회 예시
+const viewCountUrl = `http://exhibition-place.site:8080/exhibitions?searchWord=&categoryName=design&page=&size=&sort=viewCount,desc`;
+
 useEffect(() => {
-document.body.classList.toggle("active-modal", modal);
-}, [modal]);
+    let currentUrl = createdDateUrl;
 
-const sortingFunction = (a: ExhibitItem, b: ExhibitItem) => {
-if (sortType === "recent") {
-    return b.exhibitionIdx - a.exhibitionIdx;
-} else if (sortType === "mostviewed") {
-    return parseFloat(b.viewed) - parseFloat(a.viewed);
-} else if (sortType === "popular") {
-    return parseFloat(b.popular) - parseFloat(a.popular);
-}
-return 0;
-};
+    if (sortType === "recent") {
+    currentUrl = createdDateUrl;
+    } else if (sortType === "popular") {
+    currentUrl = likeCountUrl;
+    } else if (sortType === "mostviewed") {
+    currentUrl = viewCountUrl; 
+    }
 
-const sortedExhibits = [...exhibitsData].sort(sortingFunction);
-const visibleExhibits = sortedExhibits.slice(0, currentItemSet);
+    axios.get(currentUrl, {
+    headers: {
+        Authorization: 'YourAccessTokenHere',
+    }
+    })
+    .then(response => {
+    const data: APIResponse = response.data;
+    if (data.isSuccess) {
+        setExhibitsData(data.result.content);
+    } else {
+        console.error("Error fetching exhibitions:", data.message);
+    }
+    })
+    .catch(error => {
+    console.error("Error fetching exhibitions:", error);
+    });
+
+    document.body.classList.toggle("active-modal", modal);
+}, [sortType, modal]);
+
+const visibleExhibits = exhibitsData.slice(0, currentItemSet);
+
+
 
 return (
 <div>
     <Header/>
     <div style={{ width: "100%", maxWidth: "1366px", margin: "0 auto", flexDirection: "row", height: "full", display: "flex"}}>
     <div>
-    <div style={{ marginTop: "92px", marginLeft: "79px", marginRight: "681px", position: "absolute", width: "604px", height: "98px", flexShrink: "0" }}>
+        <div style={{display:"flex", flexDirection: "row"}}>
+    <div style={{ marginTop: "92px", marginLeft: "79px",  width: "605px", height: "98px", flexShrink: "0" }}>
         <div style={{ marginTop: "13px", marginLeft: "1px", marginRight: "2px", width: "605px", height: "95px", flexShrink: 0, color: "#484848", textAlign: "left", fontFamily: "Montserrat", fontSize: "36px", fontStyle: "normal", fontWeight: "700", lineHeight: "48px" }}>
         디자인 전시
-        <div style={{ marginTop: "17px", marginBottom: "24px", marginRight: "0px", display: "center", width: "150px", height: "6px", flexShrink: 0, borderRadius: "3px", backgroundColor: "#E52A2A" }}></div>
+        <div style={{ marginTop: "17px", marginBottom: "24px", marginRight: "0px", display: "center", width: "185px", height: "6px", flexShrink: 0, borderRadius: "3px", backgroundColor: "#E52A2A" }}></div>
         </div>
     </div>
-    <div>
-    <div style={{ marginLeft: "898px", marginTop: "157px", width: "385px", height: "17.2px" }}>
-        <button  onClick={() => handleButtonClick("recent")} style={{ width: "75.14px", height: "17.036px", transform: "rotate(-0.139deg)", flexShrink: "0", border: "none", backgroundColor: "white", color: "#484848", textAlign: "center", fontFamily: "Montserrat", fontSize: "20px", fontStyle: "normal", fontWeight: selectedButton === "recent" ? "600" : "500", lineHeight: "normal", cursor: "pointer" }}>Recent</button>
-        <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none" style={{ marginLeft: "16.82px", marginBottom :"2px" }}>
-            <circle cx="3" cy="3.00024" r="3" fill="#E62A2A" />
-        </svg>
-        <button onClick={() => handleButtonClick("mostviewed")} style={{ marginLeft: "16.82px", width: "145px", height: "17.036px", transform: "rotate(-0.139deg)", flexShrink: "0", border: "none", backgroundColor: "white", color: "#484848", textAlign: "center", fontFamily: "Montserrat", fontSize: "20px", fontStyle: "normal", fontWeight: selectedButton === "mostviewed" ? "600" : "500", lineHeight: "normal", cursor: "pointer" }}>Most viewed</button>
-        <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none" style={{ marginLeft: "16.82px", marginBottom:"2px" }}>
-            <circle cx="3" cy="3.00024" r="3" fill="#E62A2A" />
-        </svg>
-        <button onClick={() => handleButtonClick("popular")} style={{ marginLeft: "16.82px", width: "84.729px", height: "17.036px", transform: "rotate(-0.139deg)", flexShrink: "0", border: "none", backgroundColor: "white", color: "#484848", textAlign: "center", fontFamily: "Montserrat", fontSize: "20px", fontStyle: "normal", fontWeight: selectedButton === "popular" ? "600" : "500", lineHeight: "normal", cursor: "pointer" }}>Popular</button>
-        </div>
+        <div style={{ marginTop:"154px", marginLeft:"213px", width: "385px", height: "17.2px" }}>
+            <button  onClick={() => handleButtonClick("recent")} style={{ width: "75.14px", height: "17.036px", transform: "rotate(-0.139deg)", flexShrink: "0", border: "none", backgroundColor: "white", color: "#484848", textAlign: "center", fontFamily: "Montserrat", fontSize: "20px", fontStyle: "normal", fontWeight: selectedButton === "recent" ? "600" : "500", lineHeight: "normal", cursor: "pointer" }}>Recent</button>
+            <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none" style={{ marginLeft: "16.82px", marginBottom :"2px" }}>
+                <circle cx="3" cy="3.00024" r="3" fill="#E62A2A" />
+            </svg>
+            <button onClick={() => handleButtonClick("mostviewed")} style={{ marginLeft: "16.82px", width: "145px", height: "17.036px", transform: "rotate(-0.139deg)", flexShrink: "0", border: "none", backgroundColor: "white", color: "#484848", textAlign: "center", fontFamily: "Montserrat", fontSize: "20px", fontStyle: "normal", fontWeight: selectedButton === "mostviewed" ? "600" : "500", lineHeight: "normal", cursor: "pointer" }}>Most viewed</button>
+            <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none" style={{ marginLeft: "16.82px", marginBottom:"2px" }}>
+                <circle cx="3" cy="3.00024" r="3" fill="#E62A2A" />
+            </svg>
+            <button onClick={() => handleButtonClick("popular")} style={{ marginLeft: "16.82px", width: "84.729px", height: "17.036px", transform: "rotate(-0.139deg)", flexShrink: "0", border: "none", backgroundColor: "white", color: "#484848", textAlign: "center", fontFamily: "Montserrat", fontSize: "20px", fontStyle: "normal", fontWeight: selectedButton === "popular" ? "600" : "500", lineHeight: "normal", cursor: "pointer" }}>Popular</button>
+            </div>
     </div>
     <div style={{ marginTop: "83px", marginLeft: "80px", marginRight: "95px" }}>
+    {exhibitsData.length > 0 && (
         <div style={{ alignItems: "center", gap: "65px 60px", display: "grid", gridTemplateColumns: "repeat(3, 362px)"}}>
         {visibleExhibits.map((exhibitItem: ExhibitItem) => (
             <div key={exhibitItem.exhibitionIdx} className="exhibit" style={{ flexDirection: "column", width: "362px", height: "296px", borderRadius: "6px" }}>
@@ -121,6 +139,7 @@ return (
             </div>
         ))}
         </div>
+        )}
         <div style={{height:"90px"}}/>
     </div>
     {exhibitsData.length >= currentItemSet && (
@@ -134,7 +153,7 @@ return (
     )}
     </div>
     {selectedExhibit && (
-    <Modal exhibitItem={selectedExhibit} closeModal={handleCloseModal} style={{ width: "1145px", height: "776px" }} />
+    <Modal closeModal={handleCloseModal} style={{ width: "1145px", height: "776px" }} exhibitionIdx={selectedExhibit.exhibitionIdx} />
     )}
     </div>
     <Footer />
